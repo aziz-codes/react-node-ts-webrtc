c88553382b64d4be91e1fda485a2b907b4934eb4
aziz-codes <azizisoffline@gmail.com>
2024-11-15 18:26:42 +0500

    refactored

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,13 @@
 import  { useEffect } from 'react'
 import { useSocket } from './context/room-context';
+import JoinRoomBtn from './components/join-room-btn';
 
 const App = () => {
   const socket = useSocket();
-  useEffect(()=>{
- 
-  },[])
+   
   return (
     <div className='flex min-h-screen justify-center items-center'>
-      <button className='px-8 py-2 rounded-md bg-slate-800 text-white hover:ring-1 text-lg'>Start a new meeting</button>
+     <JoinRoomBtn />
     </div>
   )
 }

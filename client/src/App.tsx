db80af7d6bcd3cec85eb58ee00feef30f5051430
aziz-codes <azizisoffline@gmail.com>
2024-11-15 17:33:32 +0500

    sokcet connection succeed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import socket from 'socket.io-client'
 
-function App() {
+const App = () => {
+  const url = "http://localhost:8080"
+  useEffect(()=>{
+    socket(url);
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from 'cors';
 const port = 8080;
 
 const app = express();
-
+app.use(cors())
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server,{
+  cors:{
+    origin: "*",
+    methods: ["POST","GET"]
+  }
+});
 
 io.on("connection",()=>{
 console.log('user is connected')

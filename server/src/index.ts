892ed685ec7f21dbde6adab8c0f83a1adf0ea13f
aziz-codes <azizisoffline@gmail.com>
2024-11-15 16:54:14 +0500

    server basic code with socket & express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,18 @@
-console.log('hello')
-const a:number = 2;
-console.log(a)
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+const port = 8080;
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on("connection",()=>{
+console.log('user is connected')
+})
+
+server.listen(port, () => {
+  console.log(`Listening to the server on port : ${port}`);
+});

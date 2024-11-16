546e8ced2762dcdeacf91da75dcd8aaaa2286389
aziz-codes <azizisoffline@gmail.com>
2024-11-16 22:01:29 +0500

    checking socket connection

--- client/src/pages/room.tsx
+++ client/src/pages/room.tsx
@@ -1,17 +1,42 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useSocket } from '../context/room-context';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSocket } from "../context/room-context";
+ 
 const Room = () => {
-    const {id} = useParams();
+  const { id } = useParams();
+const [users,setUsers] = useState([]);
+  const { socket, me,items } = useSocket();
 
-    const socket = useSocket();
+  useEffect(() => {
+    if (me && id) {
+  
+      socket.emit("join-room", { roomId: id, peerId: me._id });
+    }
+  }, [id,me]);
 
-    useEffect(()=>{
-  socket.emit("join-room",{roomId:id})
-    },[id])
-  return (
-    <div>Room ID is {id}</div>
-  )
-}
+   
+  useEffect(()=>{
+   socket.on("get-users",(data:any)=>{
+    console.log(data); 
+  
+   if(id){
+    setUsers(data[id])
+   }
+  })
+  },[id,socket])
 
-export default Room
+ 
+
+if(!me) return <h4>Me is still loading, pelase wait for a while ...</h4>
+  return <div className="flex flex-col gap-3">
+    <h4>Room ID  {id}</h4>
+    <div className="flex flex-col gap-2">
+      <h4>users in room</h4>
+
+     <pre> {JSON.stringify(items)}</pre>
+    
+    </div>
+  </div>;
+};
+
+export default Room;
